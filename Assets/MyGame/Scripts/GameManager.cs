0f11da5a819b7db2e8a8bce0cb06dc6f4efd0251
dbraunlich <david.johannes.b@gmail.com>
2021-01-10 19:16:27 +0100

    added movement and timer

---
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI timer;
+
+    public float timeStart = 60;
+
+    private float time;
+
+    void Start()
+    {
+        time = timeStart;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+
+        timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
---
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.position += new Vector3(moveHorizontal, 0, 0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 8;
+    }
+
+}
+
